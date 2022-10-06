fe676d53de497653b3c7dbfc71f77454eedee32a
Alumno <Alumno@PC-40>
2022-10-06 12:09:42 -0500

    Agregar archivos de proyecto.

---
+++ My NotePade 06.10.22/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_NotePade_06._10._22
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
